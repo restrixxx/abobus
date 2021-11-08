852f9a82d7fa43d93850c36620061c9398281bf0
Andrejs ResTrixxx <83282216+restrixxx@users.noreply.github.com>
2021-11-08 12:46:36 +0000

    oop

---
+++ OOP.py
@@ -0,0 +1,33 @@
+saraksts = [1,2,3]
+
+print(type(saraksts))
+
+
+
+
+class Piem():
+  pass
+
+a = Piem()
+
+print(type(a))
+
+class Auto():
+  def __init__(self, modelis):
+    self.modelis=modelis
+    self.krasa = krasa
+
+Honda = Auto(modelis = "Civic EK6", krasa = "balta")
+
+Lexus = Auto(modelis = "GS300", krasa = "melna")
+
+Toyota = Auto("Supra", "sarkana")
+
+
+print(Honda.modelis)
+
+print(Lexus.modelis)
+
+print(Toyota.modelis)
+
+print(Toyota.krasa)
